98a06d3b7f721485b66b4043ac9dc01d30f514e3
Dinho e Roberta <Dinho e Roberta>
2017-10-14 22:52:54 -0300

    adicionar tudo

---
+++ www/js/services.js
@@ -0,0 +1,61 @@
+(function(angular) {
+
+    var app = angular.module('saude');
+
+
+    app.service('Sparql', function($http, $q, $sce) {
+
+        var PARAMS = {"default-graph-uri": "http://dbpedia.org", "output": "json", "callback": "JSON_CALLBACK"};
+        var URL = "http://dbpedia.org/sparql";
+
+        this.getQuery = function(queryName) {
+            var q = $q.defer();
+
+            $http.get('queries/' + queryName + '.rq').then(function(result){
+                q.resolve(result.data);
+            });
+
+            return q.promise;
+        };
+
+        this.execute = function(queryName, params) {
+            var q = $q.defer();
+            var that = this;
+            this.getQuery(queryName).then(function(txt) {
+
+                var data = angular.extend({}, PARAMS, {
+                    query: that.format(txt, params)
+                });
+
+                console.log(data.query);
+
+                $http.jsonp(URL, {params: data}).then(function(result) {
+                    q.resolve(result.data.results);
+                }, function(err) {
+                    q.reject(err);
+                });
+            });
+
+            return q.promise;
+        };
+
+        this.format = function(txt, params) {
+            Object.keys(params).forEach(function(key) {
+                if (angular.isObject(params[key])) {
+                    // {txt: string, values: array, joiner: string}
+                    var txt = params[key].txt;
+                    params[key] = params[key].values.map(function(value) {
+                        return format(txt, value.replace(/\'/gi, "\\'"));
+                    }).join(params[key].joiner);
+                } else {
+                    params[key] = params[key].replace(/\'/gi, "\\'");
+                }
+            });
+
+            return format(txt, params);
+        };
+
+
+    });
+
+})(angular);
